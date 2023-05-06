1cc2682af6cb0cfa3c9a132d295c4e9663250624
pparksang1013 <pparksang1013@users.noreply.github.com>
2023-05-06 16:18:22 +0900

    Add HeaderComponents

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,43 +1,31 @@
 import { Routes, Route } from "react-router-dom";
 import styled from "styled-components";
 
-
-
 // COMPONENT
-import Dashboard from "./router/Dashboard";
+import DashboardRouter from "./router/DashboardRouter";
 import CalendarRouter from "./router/CalendarRouter";
 import Board from "./pages/Board";
-
-
-
+import BoardRouter from "./router/BoardRouter";
+import HeaderComponents from "./components/HeaderComponents";
 
 const Wrapper = styled.div`
-display:flex
+    display: flex;
 `;
 
 function App() {
-
-  // useEffect(() => {
-  //   async function fetchdata() {
-  //     const { data } = await axios.get('/users');
-  //     console.log(data);
-  //   }
-  //   fetchdata();
-  // }, []);
-
-
-  return (
-    <div className="App">
-      <Wrapper>
-        <Routes >
-          <Route path="/" element={<Board />}/>
-          <Route path="/calendar" element={<CalendarRouter />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/kanbanboard" element={<Board />} />
-        </Routes>
-      </Wrapper>
-</div>
-  );
+    return (
+        <div className="App">
+            <Wrapper>
+                <HeaderComponents />
+                <Routes>
+                    <Route path="/" element={<BoardRouter />} />
+                    <Route path="/calendar" element={<CalendarRouter />} />
+                    <Route path="/dashboard" element={<DashboardRouter />} />
+                    <Route path="/kanbanboard" element={<Board />} />
+                </Routes>
+            </Wrapper>
+        </div>
+    );
 }
 
 export default App;
