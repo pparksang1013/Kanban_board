c240c337767b96634af82c8a5e57340a59cae6b2
pparksang1013 <pparksang1013@users.noreply.github.com>
2023-05-06 15:19:56 +0900

    Add styled components theme추가

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import theme from "./style/theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <ThemeProvider theme={theme}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </ThemeProvider>
     </React.StrictMode>
 );
