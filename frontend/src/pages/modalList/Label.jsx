97485b6aaca4d76adce354135cb6c86067cc05e3
pparksang1013 <pparksang1013@users.noreply.github.com>
2023-05-10 11:06:45 +0900

    현희 페이지

--- frontend/src/pages/modalList/Label.jsx
+++ frontend/src/pages/modalList/Label.jsx
@@ -1,56 +1,49 @@
 import React, { useState } from "react";
 import Modal from "react-modal";
-import * as A from "../../style/test";
+import * as A from "../style/test";
 
 function Category() {
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    // 모달창 Open
-    const openModal = () => {
-        setIsOpen(true);
-    };
-    // 모달창 Close
-    const closeModal = () => {
-        setIsOpen(false);
-    };
-    return (
-        <div>
-            <A.Button onClick={openModal} className="plus">
-                Label
-            </A.Button>
-            <Modal
-                isOpen={isOpen}
-                onRequestClose={closeModal}
-                ariaHideApp={false}
-                style={{
-                    overlay: {
-                        backgroundColor: "rgba(0, 0, 0, 0.5)",
-                    },
-                    content: {
-                        width: "300px",
-                        height: "300px",
-                        margin: "auto",
-                    },
-                }}
-            >
-                <A.Div className="modal_box">
-                    <A.Div className="modal_top">
-                        <A.Button onClick={closeModal} className="btn_close">
-                            {" "}
-                            X{" "}
-                        </A.Button>
-                        <A.Modal_Title> Label</A.Modal_Title>
-                    </A.Div>
-                    <A.Div className="modal_middle">
-                        <A.Input type="checkbox"></A.Input>
-                    </A.Div>
-                    <A.Div className="modal_bottom">
-                        <A.Input type="button" value="라벨추가"></A.Input>
-                    </A.Div>
-                </A.Div>
-            </Modal>
-        </div>
-    );
+  // 모달창 Open
+  const openModal = () => {
+    setIsOpen(true);
+  };
+  // 모달창 Close
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+  return (
+    <div>
+      <A.Button onClick={openModal} className="plus">
+        Label
+      </A.Button>
+      <Modal isOpen={isOpen} onRequestClose={closeModal} ariaHideApp={false} style={{
+        overlay: {
+          backgroundColor: "rgba(0, 0, 0, 0.5)",
+        },
+        content: {
+          width: "300px",
+          height: "300px",
+          margin: "auto",
+        },
+      }}
+      >
+        <A.Div className="modal_box">
+          <A.Div className="modal_top">
+            <A.Button onClick={closeModal} className="btn_close"> X </A.Button>
+            <A.Modal_Title> Label</A.Modal_Title>
+          </A.Div>
+          <A.Div className="modal_middle">
+            <A.Input type="checkbox"></A.Input>
+          </A.Div>
+          <A.Div className="modal_bottom">
+            <A.Input type="button" value="라벨추가"></A.Input>
+          </A.Div>
+        </A.Div>
+      </Modal>
+    </div>
+  );
 }
 
 export default Category;
