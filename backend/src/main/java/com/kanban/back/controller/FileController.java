5f8e74a1657e797e64e56e467b3944bc15e0874e
Donggyu Lee <dongco213@naver.com>
2023-05-10 11:31:06 +0900

    feat FileController.java

--- backend/src/main/java/com/kanban/back/controller/FileController.java
+++ backend/src/main/java/com/kanban/back/controller/FileController.java
@@ -55,7 +55,7 @@ public class FileController {
 
         return ResponseEntity.ok()
                 .contentType(MediaType.parseMediaType(contentType))
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+//                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
                 .body(resource);
     }
 }
