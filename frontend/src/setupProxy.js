6761e59d5dc35219a92efd4d5d2bcbd14935256e
pparksang1013 <pparksang1013@users.noreply.github.com>
2023-05-17 11:21:12 +0900

    Feat proxy

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,11 +1,11 @@
 // src/setupProxy.js
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
-  app.use(
-    createProxyMiddleware('/users', {
-      target: 'http://localhost:9080',
-      changeOrigin: true,
-    }),
-  );
+    app.use(
+        createProxyMiddleware("/users", {
+            target: "http://localhost:9080",
+            changeOrigin: true,
+        })
+    );
 };
